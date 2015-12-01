aaa30767a2914a1d047c09cf385e387ae9146aa1
Will Pillar <will.pillar@graze.com>
2015-12-01 15:38:22 +0000

    Basic REST adapter

---
+++ src/Adapter/Http/Rest/Configuration/Configuration.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Graze\Dal\Adapter\Http\Rest\Configuration;
+
+use Graze\Dal\Adapter\Http\Rest\Persister\EntityPersister;
+use Graze\Dal\Configuration\AbstractConfiguration;
+use Graze\Dal\Configuration\ConfigurationInterface;
+use Graze\Dal\DalManagerInterface;
+use Graze\Dal\Persister\PersisterInterface;
+use Graze\Dal\UnitOfWork\UnitOfWork;
+use Graze\Dal\UnitOfWork\UnitOfWorkInterface;
+use GuzzleHttp\ClientInterface;
+
+class Configuration extends AbstractConfiguration
+{
+    /**
+     * @var ClientInterface
+     */
+    private $client;
+
+    /**
+     * @param DalManagerInterface $dalManager
+     * @param ClientInterface $client
+     * @param array $mapping
+     * @param $trackingPolicy
+     */
+    public function __construct(DalManagerInterface $dalManager, ClientInterface $client, array $mapping, $trackingPolicy = UnitOfWork::POLICY_EXPLICIT)
+    {
+        parent::__construct($dalManager, $mapping, $trackingPolicy);
+        $this->client = $client;
+    }
+
+    /**
+     * @param string $entityName
+     * @param ConfigurationInterface $config
+     *
+     * @return string
+     */
+    protected function getRecordName($entityName, ConfigurationInterface $config)
+    {
+        $mapping = $config->getMapping($entityName);
+        return $mapping['resource'];
+    }
+
+    /**
+     * @param string $entityName
+     * @param ConfigurationInterface $config
+     * @param UnitOfWorkInterface $unitOfWork
+     *
+     * @return PersisterInterface
+     */
+    protected function buildDefaultPersister(
+        $entityName,
+        ConfigurationInterface $config,
+        UnitOfWorkInterface $unitOfWork
+    ) {
+        return new EntityPersister($entityName, $this->getRecordName($entityName, $config), $unitOfWork, $config, $this->client);
+    }
+}
---
+++ src/Adapter/Http/Rest/RestAdapter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Graze\Dal\Adapter\Http\Rest;
+
+use Graze\Dal\Adapter\AdapterInterface;
+use Graze\Dal\Adapter\Http\AbstractHttpAdapter;
+
+class RestAdapter extends AbstractHttpAdapter implements AdapterInterface
+{
+}
