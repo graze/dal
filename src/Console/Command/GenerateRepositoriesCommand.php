14e725caeac0a4bf4354b755023b50449dfae0ab
Will Pillar <will.pillar@graze.com>
2016-12-24 14:19:38 +0000

    Optionally generate an interface for each repository

--- src/Console/Command/GenerateRepositoriesCommand.php
+++ src/Console/Command/GenerateRepositoriesCommand.php
@@ -6,6 +6,7 @@ use Graze\Dal\Generator\RepositoryGenerator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Parser;
 
@@ -19,7 +20,8 @@ class GenerateRepositoriesCommand extends Command
             ->setDescription('Use provided config to generate repositories.')
             ->addArgument('config', InputArgument::REQUIRED, 'The YAML config file to generate from.')
             ->addArgument('root-namespace', InputArgument::REQUIRED, 'The root namespace for the repositories (e.g. Acme\\\\Entity).')
-            ->addArgument('directory', InputArgument::REQUIRED, 'The source directory for the generated repositories');
+            ->addArgument('directory', InputArgument::REQUIRED, 'The source directory for the generated repositories')
+            ->addOption('interfaces', null, InputOption::VALUE_NONE, 'Generate an interface for each repository.');
     }
 
     /**
@@ -31,7 +33,7 @@ class GenerateRepositoriesCommand extends Command
         $configPath = $input->getArgument('config');
         $config = (new Parser())->parse(file_get_contents($configPath));
 
-        $generator = new RepositoryGenerator($config);
+        $generator = new RepositoryGenerator($config, $input->getOption('interfaces'));
         $repositories = $generator->generate();
 
         $rootNamespace = $input->getArgument('root-namespace');
--- src/Generator/RepositoryGenerator.php
+++ src/Generator/RepositoryGenerator.php
@@ -4,6 +4,7 @@ namespace Graze\Dal\Generator;
 
 use Zend\Code\Generator\ClassGenerator;
 use Zend\Code\Generator\FileGenerator;
+use Zend\Code\Generator\InterfaceGenerator;
 
 class RepositoryGenerator implements GeneratorInterface
 {
@@ -13,11 +14,18 @@ class RepositoryGenerator implements GeneratorInterface
     private $config;
 
     /**
-     * @param array $config
+     * @var bool
      */
-    public function __construct(array $config)
+    private $generateInterfaces;
+
+    /**
+     * @param array $config
+     * @param bool $generateInterfaces
+     */
+    public function __construct(array $config, $generateInterfaces = false)
     {
         $this->config = $config;
+        $this->generateInterfaces = $generateInterfaces;
     }
 
     /**
@@ -35,6 +43,17 @@ class RepositoryGenerator implements GeneratorInterface
             $repository = new ClassGenerator();
             $repository->setName($config['repository']);
             $repository->setExtendedClass('\\Graze\Dal\Repository\EntityRepository');
+
+            if ($this->generateInterfaces) {
+                $interfaceName = $config['repository'] . 'Interface';
+                $repositoryInterface = new InterfaceGenerator();
+                $repositoryInterface->setName($interfaceName);
+
+                $file = FileGenerator::fromArray(['classes' => [$repositoryInterface]]);
+                $repositories[$interfaceName] = rtrim(preg_replace('/\n(\s*\n){2,}/', "\n\n", $file->generate()), "\n") . "\n";
+
+                $repository->setImplementedInterfaces(['\\' . $interfaceName]);
+            }
 
             $file = FileGenerator::fromArray(['classes' => [$repository]]);
             $repositories[$config['repository']] = rtrim(preg_replace('/\n(\s*\n){2,}/', "\n\n", $file->generate()), "\n") . "\n";
